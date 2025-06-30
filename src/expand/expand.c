353b4a623179be11d8dddb0d8e46304c441fe485
Burak Tuncer <btuncer@k2m12s04.42kocaeli.com.tr>
2025-06-30 19:14:51 +0300

    exp

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -6,23 +6,52 @@
 /*   By: btuncer <btuncer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 16:28:30 by btuncer           #+#    #+#             */
-/*   Updated: 2025/06/27 16:42:30 by btuncer          ###   ########.fr       */
+/*   Updated: 2025/06/30 19:14:40 by btuncer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./expand.h"
+#include "./../libft/libft.h"
 
-void expand_str(char *str)
+#define EXPANDER_ESC_CHS "' "
+
+typedef struct s_command t_command;
+typedef struct s_command {
+    char        **args;
+    int         argc;
+};
+
+void *alloc(ssize_t size);
+
+char *trim_expandable(char *str)
 {
-    t_env *env;
-    char *ptr;
+    int counter;
+    int check;
+    char *expandable;
 
-    ptr = str;
-    env = get_env();
-    while (*str)
+    counter = 0;
+    while (str[counter] != '$')
+        counter++;
+    check = counter;
+    while (!in(EXPANDER_ESC_CHS, str[counter]) && str[counter])
+        counter++;
+    expandable = alloc((counter - check + 2) * sizeof(char));
+    strncmp(src=str, dest=expandable, start=check, n=counter-check+1);
+}
+char *expand_str(char *str)
+{
+    
+}
+
+void expand_command(t_command *command)
+{
+    int counter;
+
+    counter = 0;
+    while (counter < command->argc)
     {
-        if (*str == '$') {
-            printf("aha\n"); *str = '%'; }
-        str++;
+        if (in(command->args[counter], '$'))
+            expand_str(command->args[counter]);
+        counter++;
     }
 }
